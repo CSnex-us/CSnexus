7b9da0b70e401914586157a2b5091d020f3c4101
Stephen Maher <srqcoder@gmail.com>
2022-01-06 16:57:46 -0500

    create hello app

---
+++ hello/views.py
@@ -0,0 +1,20 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, CSNexus!")
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
